e868c9861464fc62fd62fdd90a276925e79b5c61
Alex Braun <alexhbraun@gmail.com>
2026-02-09 11:31:42 -0300

    Updated logo image

--- components/PestAnswerLogo.tsx
+++ components/PestAnswerLogo.tsx
@@ -5,7 +5,7 @@ export const PestAnswerLogo = ({ className = "" }: { className?: string }) => {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
       <img 
-        src="https://ik.imagekit.io/rgqefde41/PestAnswer%20logo.png" 
+        src="https://ik.imagekit.io/rgqefde41/Design%20sem%20nome%20(12).png" 
         alt="PestAnswer Logo" 
         className="h-20 w-auto object-contain rounded-lg shadow-sm"
       />
